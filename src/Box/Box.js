4f26fc2816a163cf407b51cfce344c467f7856ca
jger <j.gerardis@gmail.com>
2019-05-05 09:16:03 +0200

    Show different when box is free

--- src/Box/Box.js
+++ src/Box/Box.js
@@ -4,10 +4,11 @@ import './Box.css'
 
 const box = (props) => {
 
+    let className = (props.value!=='X' && props.value!=='O') ? 'Box free':'Box';
 
     return (
 
-        <div className='Box' onClick={props.clicked}>
+        <div className={className} onClick={props.clicked}>
             {props.value}
         </div>
 
